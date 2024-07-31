3276f51841216e19ad4f6229282836da05b247d2
Katheesh1224 <kgobi24lk@gmail.com>
2024-07-31 09:44:50 +0530

    .

--- gyn/src/Register_staff.js
+++ gyn/src/Register_staff.js
@@ -6,7 +6,6 @@ import Nav from './component/Nav.jsx';
 import NavBar from './component/NavBar.jsx';
 
 const RegisterStaff = () => {
-  const navigate = useNavigate();
   const initialState = {
     full_name: "",
     phone_no: "",
