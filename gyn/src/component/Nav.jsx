70cc41da2c7fefcbb4834e7ded525f58dbb3f76a
Nerujan <sathasivamnerujan35@gmail.com>
2024-10-03 21:35:30 +0530

    role finished check

--- gyn/src/component/Nav.jsx
+++ gyn/src/component/Nav.jsx
@@ -4,23 +4,26 @@ import { faHouse, faRectangleList, faHospitalUser, faUserNurse } from '@fortawes
 import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
+  const role = localStorage.getItem('role');
   return (
     <div>
       <header id="header" className="d-flex flex-column justify-content-center">
         <nav id="navbar" className="navbar nav-menu">
           <ul>
             <li>
-              <NavLink exact to="/home" className="nav-link" activeClassName="active">
+              <NavLink exact to={role==="data_entry" ? '/backup' : '/home'} className="nav-link" activeClassName="active">
                 <FontAwesomeIcon icon={faHouse} />
                 <span>Home</span>
               </NavLink>
             </li>
+            {role !== 'data_entry' && (
             <li>
               <NavLink to="/patient_registration" className="nav-link" activeClassName="active">
                 <FontAwesomeIcon icon={faRectangleList} />
                 <span>Patient Registration</span>
               </NavLink>
             </li>
+            )}
             <li>
               <NavLink to="/patients_information" className="nav-link" activeClassName="active">
                 <FontAwesomeIcon icon={faHospitalUser} />
